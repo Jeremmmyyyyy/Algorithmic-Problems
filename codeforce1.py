25785d1033cf019df5e262c83e45bc2b17abb1c3
@barghorn <jeremy.barghorn@epfl.ch>
2022-12-10 18:09:32 +0100

    test 51 failed

--- codeforce1.py
+++ codeforce1.py
@@ -14,18 +14,18 @@ def getInput():
 
 def memorized_mad_max_aux(posts, gasAvailable, M, T, maxVal):
     index = posts - 1
-    alreadyComp = [(gas,food) for gas, food in M[index] if gas  == gasAvailable]
+    if M[index][gasAvailable] != -1:
+        return M[index][gasAvailable] 
+    # if len(alreadyComp) == 1:
+    #     return alreadyComp[0][1]
+    # elif len(alreadyComp) > 1:
+    #     maxFood = -1
+    #     for elem in alreadyComp:
+    #         if elem[1] >= maxFood:
+    #             maxFood = elem[1]
+    #     return maxFood
     
-    if len(alreadyComp) == 1:
-        return alreadyComp[0][1]
-    elif len(alreadyComp) > 1:
-        maxFood = -1
-        for elem in alreadyComp:
-            if elem[1] >= maxFood:
-                maxFood = elem[1]
-        return maxFood
-    
-    if posts == maxVal:
+    if posts >= maxVal:
         result = 0
     else:
         result = -1
@@ -36,8 +36,11 @@ def memorized_mad_max_aux(posts, gasAvailable, M, T, maxVal):
             if gasAvailable > 0:
                 result1 = memorized_mad_max_aux(posts + 1, gasAvailable - 1, M, T, maxVal)
             for i in range(len(T[index])):
-                if gasAvailable + T[index][i][3]>= T[index][i][1] - T[index][i][0]:
-                    result2 = memorized_mad_max_aux(T[index][i][1], gasAvailable - (T[index][i][1] - T[index][i][0]) + T[index][i][3], M, T, maxVal)
+                currentGasPlusHhGas = gasAvailable + T[index][i][3]
+                if currentGasPlusHhGas >= T[index][i][1] - T[index][i][0]:
+                    if currentGasPlusHhGas >= maxVal - index:
+                        currentGasPlusHhGas = maxVal - index
+                    result2 = memorized_mad_max_aux(T[index][i][1], currentGasPlusHhGas - (T[index][i][1] - T[index][i][0]), M, T, maxVal)
                     foodBonus = T[index][i][2]
                     if result2 + foodBonus < foodBonus:
                         result = -1
@@ -48,12 +51,8 @@ def memorized_mad_max_aux(posts, gasAvailable, M, T, maxVal):
         else:
             result = -1
 
-    # updateRes = [idx for idx, tup in enumerate(alreadyComp) if tup[0] == gasAvailable]
-    # if len(updateRes) > 0:
-    #     if M[index][updateRes[0]][1] < result:
-    #         M[index][updateRes[0]][1] = result
-    # else:
-    M[index].append((gasAvailable, result))
+    if M[index][gasAvailable] < result:
+        M[index][gasAvailable] = result
     return result
 
 
@@ -62,17 +61,26 @@ def memorized_mad_max(n_posts, gas, hitchhickers):
         return 0
     else:
         newTable = []
-        m = []
+        m = [[-1 for x in range(n_posts)] for y in range(n_posts)] 
         for i in range(n_posts):
             newTable.append([])
-            m.append([(-1, -1)])
+        maxFood = 0
+        maxFuel = gas
         for index, value in enumerate(hitchhickers):
+            maxFood += value[2]
+            maxFuel += value[3]
             newTable[value[0] - 1].append(value)
             if value[0] == value [1]:
                 return - 1
-        if gas >= n_posts:
-            gas = n_posts
-        return memorized_mad_max_aux(1, gas, m, newTable, n_posts)
+        if gas >= n_posts - 1:
+            gas = n_posts - 1
+
+        if maxFuel < n_posts - 1:
+            return -1
+        elif maxFood == 0 and maxFuel >= n_posts:
+            return 0
+        else:
+            return memorized_mad_max_aux(1, gas, m, newTable, n_posts)
 
 def tests(bool = False):
     if bool:
@@ -249,6 +257,26 @@ def tests(bool = False):
         res = memorized_mad_max(n_posts, gas, hitchhickers)
         assert res == -1
 
+        n_posts = 10
+        gas = 20
+        hitchhickers = [(2, 4, 5, 20), (2, 4, 4, 25), (4, 10, 0, 0)]
+        res = memorized_mad_max(n_posts, gas, hitchhickers)
+        assert res == 5
+
+        n_posts = 10
+        gas = 20
+        hitchhickers = [(2, 4, 4, 20), (2, 4, 5, 25), (4, 10, 0, 0)]
+        res = memorized_mad_max(n_posts, gas, hitchhickers)
+        assert res == 5
+
+        # edge case avant de lancer la recurrence 
+        
+        # if maxFuel < n_posts - 1:
+        #     return -1
+        # elif maxFood == 0 and maxFuel >= n_posts:
+        #     return 0
+
+        # capper le gaz au max de posts
         print("All ok")
 
         exit()
@@ -256,7 +284,7 @@ def tests(bool = False):
 
 if __name__ == '__main__':
 
-    tests(True)
+    tests(False)
 
     try:
         n_posts, n_hitch, gas, hitchhickers = getInput()
