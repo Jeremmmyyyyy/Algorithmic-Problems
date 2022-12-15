a7905ab476067a886bb89bac29ed3bd0607365f3
@barghorn <jeremy.barghorn@epfl.ch>
2022-12-15 18:32:41 +0100

    FINISHED

--- codeforce_problem_B.py
+++ codeforce_problem_B.py
@@ -1,4 +1,5 @@
 import heapq
+import math
 
 #* Input management
 def getInput():
@@ -33,6 +34,7 @@ def adjacencyList(n_intersections, roads):
     adjList = [[] for i in range(n_intersections)]
     for road in roads:
         adjList[road[2] - 1].append((road[3], road[0]))
+        adjList[road[3] - 1].append((road[2], road[0]))
     return adjList
 
 def optimized_path(intersections, n_roads, start, end, roads):
@@ -52,41 +54,59 @@ def optimized_path(intersections, n_roads, start, end, roads):
     maxSnow = -1
     for i in range(n_roads):
         newElem = heapq.heappop(heapqSnow)
+        if maxSnow < newElem[0] and findSet(connectedComp, start - 1) == findSet(connectedComp, end - 1):
+            break
+        maxSnow = newElem[0]
         union(connectedComp, newElem[2] - 1, newElem[3] - 1)
         #* pushes the roads that are valid in the heapq ordered by length.
         lengthList.append((newElem[1], newElem[0], newElem[2], newElem[3]))
-        if findSet(connectedComp, start - 1) == findSet(connectedComp, end - 1):
-            maxSnow = newElem[0]
-            break
+
+    if findSet(connectedComp, start - 1) != findSet(connectedComp, end - 1):
+        print("Impossible")
+        exit()
         
     #* creates adjList to easier access to the neighbor vertex when uisng djikstra
     adjList = adjacencyList(intersections, lengthList)
     #* init the mist with max val
     Q = []
     #* init the source to 0
-    heapq.heappush(Q, (0, start - 1))
-    for i in range(1, intersections):
-        heapq.heappush(Q, (10**10, i))
-    S = []
+    for i in range(1, intersections + 1):
+        if i == start:
+            heapq.heappush(Q, (0, i))
+        else:
+            heapq.heappush(Q, (math.inf, i))
+
+    distances = [math.inf for i in range(intersections)]
+    distances[start-1] = 0
+
     while len(Q) != 0:
         u = heapq.heappop(Q)
-        S.append(u)
-        for dest in adjList[u[1]]:
-            # tup = [end for i in Q if i[1] == dest[0] - 1]
-            if Q[dest[0] - 1][0] > u[0] + dest[1]:
-                Q[dest[0] - 1] = (u[0] + dest[1], Q[dest[0] - 1][1])
+
+        for dest in adjList[u[1] - 1]:
+            if distances[dest[0] - 1] > distances[u[1] - 1] + dest[1]:
+                distances[dest[0] - 1] = distances[u[1] - 1] + dest[1]
+                heapq.heappush(Q, (distances[dest[0] - 1], dest[0]))
     
-    length = [length for (length, id) in Q if id == end]
-    return maxSnow, end
+    return maxSnow, distances[end - 1]
 
 if __name__ == '__main__':
     #* get the input from stdin
-    # intersections, n_roads, start, end, roads = getInput()
-    intersections = 3
-    n_roads = 3
-    start = 1 
-    end = 3
-    roads = [(1, 3, 1, 3), (1, 2, 10, 1), (2, 3, 10, 2)]
-    optimized_path(intersections, n_roads, start, end, roads)
-    
-        
+    intersections, n_roads, start, end, roads = getInput()
+
+    # intersections = 4
+    # n_roads = 4
+    # start = 1
+    # end = 4
+    # roads=[(1, 3 , 100, 1),(1,2,2,1),(3,4,1,2),(2,3,2,2)]
+
+    if start == end:
+        print(0, 0)
+        exit()
+    if n_roads == 0:
+        print("Impossible")
+        exit()
+    maxSnow, distance = optimized_path(intersections, n_roads, start, end, roads)    
+    if distance == math.inf:
+        print("Impossible")
+    else:
+        print(maxSnow, distance)
